e8a39af9663621dc32d3d6d0d324cf119b16a581
Om Rajgure <omrajgure@Oms-MacBook-Pro.local>
2025-05-25 23:52:37 +0530

    added config in gitignore

--- frontend/firebase-messaging-sw.js
+++ frontend/firebase-messaging-sw.js
@@ -1,16 +1,8 @@
 
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
+importScripts("/firebase_config.js");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCrc0SUbStdZlqk6Gb2X2UzVU0aRjhPU54",
-  authDomain: "webnotificationsystem.firebaseapp.com",
-  projectId: "webnotificationsystem",
-  storageBucket: "webnotificationsystem.appspot.com",
-  messagingSenderId: "593356455427",
-  appId: "1:593356455427:web:88163ce5ec0356d48a1836",
-  measurementId: "G-CPVG2BNCE9"
-};
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
