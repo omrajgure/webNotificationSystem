d93d2fe33beca04f8d9a37c4b497b1f1c2753412
Om Rajgure <omrajgure@Oms-MacBook-Pro.local>
2025-05-26 07:07:23 +0530

    sending config to git

---
+++ frontend/firebase_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrc0SUbStdZlqk6Gb2X2UzVU0aRjhPU54",
+    authDomain: "webnotificationsystem.firebaseapp.com",
+    projectId: "webnotificationsystem",
+    storageBucket: "webnotificationsystem.appspot.com",
+    messagingSenderId: "593356455427",
+    appId: "1:593356455427:web:88163ce5ec0356d48a1836",
+    measurementId: "G-CPVG2BNCE9"
+  };
